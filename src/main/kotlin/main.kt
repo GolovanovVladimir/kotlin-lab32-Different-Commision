d113ced45e7aa66970f1cc8216e10e3c3e7b7b9d
Vladimir Golovanov2 <golovanov.vladimir.ivanovich@gmail.com>
2023-01-27 01:35:30 +0300

    New version

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,15 @@
 package ru.netology
 
+val minAmount = 0
+
+
 fun main() {
     var flag = -1
-    var percentCommission = 0.75
-    val minAmount = 35
-//    var input : String
+    var typeCard = "VK Pay"
 
     println("Программа расчета при переводе")
     while (flag == -1) {
-        println("Выберите сумму операции в рублях: ")
+        println("Выберите сумму операции текущей операции в рублях: ")
         println("0-Выход из программы")
 
         var input = readLine()!!
@@ -17,13 +18,96 @@ fun main() {
                 flag = 0; break
             }
         }
-        var amount: Int = input.toInt()
-        println("В планируете перечислить $amount  руб.")
-        if (amount >= minAmount) println("Комиссия банка составит ${amount /100 * percentCommission} руб.")
-        else {
-            println("Операция невозможна: сумма меньше минимума")
+        var amountCurrent: Double = input.toDouble()
+
+        println("Выберите тип карты: ")
+        println("1-MASTER / MAESTRO")
+        println("2-VISA / МИР")
+        println("3-VK Pay (по умолчанию)")
+        println("0-Выход из программы")
+
+        input = readLine()!!
+        when (input) {
+            "0" -> {
+                flag = 0; break
+            }
+            "1" -> typeCard = "MASTER / MAESTRO"
+            "2" -> typeCard = "VISA / МИР"
+            "3" -> typeCard = "VK Pay"
         }
-        println("")
+
+
+        calculateCommission(typeCard, 37_000.0 , amountCurrent)
+
     }
  println("Программа расчета процентов закончила свою работу.")
+}
+
+fun calculateCommission(cardType :String = "VK Pay", amountsPrevious : Double = 0.0 ,amountCurrent: Double ) {
+    var minMasterLimit = 300
+    var maxMasterLimit = 75_000
+    var percentMasterCommission = 0.6
+    var amountMasterCommission = 20.0
+    var amountCommission = 0.0
+    var minVisaLimit = 35.0
+    var percentVisaCommission = 0.75
+    var maxVKPayOperation = 15_000
+    var maxVKPayMonthOperation = 40_000
+    var limitOperation = 150_000.0
+    var limitMonth = 600_000.0
+    var flag = 0
+
+    when (cardType) {
+        "MASTER / MAESTRO" -> {
+            if ((amountCurrent < minMasterLimit) || (amountCurrent > maxMasterLimit)) {
+                amountCommission = amountCurrent / 100 * percentMasterCommission + amountMasterCommission
+            } else {
+                amountCommission = 0.0
+            }
+        }
+
+        "VISA / МИР" -> {
+            if (amountCurrent > minVisaLimit) {
+                amountCommission = amountCurrent / 100 * percentVisaCommission
+            } else {
+                println("Операция невозможна : операция меньше $minVisaLimit по карте $cardType!")
+                flag = 2
+            }
+        }
+
+        else -> amountCommission =0.0
+   }
+
+    if (cardType == "MASTER / MAESTRO"){
+        if (limitOperation > amountCurrent) {
+            if (limitMonth > (amountCurrent + amountsPrevious)) {
+                println("В планируете перечислить с карты $cardType сумму $amountCurrent  руб., ранее перечисляли $amountsPrevious руб.")
+                println("Комиссия по операции $amountCommission руб.")
+                println("")
+            } else println("Операция невозможна : превышен лимит по месяцу по карте $cardType!")
+        } else println("Операция невозможна : превышен лимит на операцию по карте $cardType!")
+    }
+
+    if ((cardType == "VISA / МИР")&& (flag!=2)){
+        if (limitOperation > amountCurrent) {
+            if (limitMonth > (amountCurrent + amountsPrevious)) {
+                println("В планируете перечислить с карты $cardType сумму $amountCurrent  руб., ранее перечисляли $amountsPrevious руб.")
+                println("Комиссия по операции $amountCommission руб.")
+                println("")
+            } else println("Операция невозможна : превышен лимит по месяцу по карте $cardType!")
+        } else println("Операция невозможна : превышен лимит на операцию по карте $cardType!")
+    }
+
+
+        if (cardType == "VK Pay") {
+            if (maxVKPayOperation > amountCurrent) {
+                if (maxVKPayMonthOperation > (amountCurrent+amountsPrevious)) {
+                    println("В планируете перечислить с карты $cardType сумму $amountCurrent  руб., ранее перечисляли $amountsPrevious руб.")
+                    println("Комиссия по операции $amountCommission руб.")
+                    println("")
+                }
+                else println("Операция невозможна : превышена макс.сумма по месяцу по карте $cardType!")
+            }
+            else println("Операция невозможна : превышена макс.сумма на операцию по карте $cardType!")
+        }
 }
